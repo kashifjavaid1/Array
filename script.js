b1317da3dcd461eae907354c95360d08b24b8ca8
kashifjavaid1 <gmdcf797@gmail.com>
2023-05-10 21:40:44 +0500

    array create
    for loop use all array name print

---
+++ script.js
@@ -0,0 +1,18 @@
+// var names=["kashif","ali","usama","abubaker","umair","umer","mateen","junaid","sohail","abduallah","sohaid"];
+// // document.write("name=>",names[5]);
+// // names[3]="muhammad kashif javaid";
+// // console.log(names[3]);
+// // console.log(names.length);
+// for (let index = 0; index < names.length; index++) {
+//     console.log("index",index);
+//   console.log("names",names[index]);
+    
+// }
+
+// var number=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,38,39,40];
+// // console.log(number[15]);
+// for (var index = 0; index < number.length; index++) {
+//    console.log("index",index);
+//    console.log("Numbers",number[index]);
+    
+// }
